62f2b032990a019842c77c3dc067b200e9168b19
dewey <liu_yang_apply.163.com>
2026-07-17 11:05:10 +0800

    当inputD为x.00时为球镜，当为x.45时为柱镜

--- configpara.py
+++ configpara.py
@@ -44,7 +44,7 @@ class configparameters():
             print(f"视场角方向direction:{self.direction}")
     
     def makestrforsave(self):
-         return f"入瞳半径：{self.ri:.2f}，出瞳半径：{self.ro:.2f}，视场角：{transdirstr[self.direction]}，偏转X：{self.angle:.1f}，偏转Y：{self.angle:.1f}，试镜片：{int(self.inputD)}"
+         return f"入瞳半径：{self.ri:.2f}，出瞳半径：{self.ro:.2f}，视场角：{transdirstr[self.direction]}，偏转X：{self.angle:.1f}，偏转Y：{self.angle:.1f}，试镜片：{self.inputD:.2f}"
     def setconfigparameters(self,ri,ro,theta,angle,lens):
         with self._lock:
             self.ri = ri
